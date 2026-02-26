64b2385f3470d8cf3b33d5911b117c0e99b7079d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-02-26 21:03:25 +0200

    Add VoteStats

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,10 +2,12 @@
 import { useState } from 'react';
 import CafeInfo from '../CafeInfo/CafeInfo';
 
-import css from './App.module.css';
-import VoteOpptions from '../VoteOptions/VoteOptions.tsx';
-
 import { type Votes, type VoteType } from '../../types/votes.ts';
+
+import css from './App.module.css';
+
+import VoteOpptions from '../VoteOptions/VoteOptions.tsx';
+import VoteStats from '../VoteStats/VoteStats.tsx';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -30,14 +32,25 @@ export default function App() {
     }
   }
 
-  function resetVotes() {
+  function resetVotes(): void {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   }
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
     <div className={css.app}>
       <CafeInfo />
       <VoteOpptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
     </div>
   );
 }
